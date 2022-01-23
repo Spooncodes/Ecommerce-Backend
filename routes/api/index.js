333ec069b4af9cd89bf2bc19ce5a18032bd5d6bb
Spoon God <spoon.is.god@gmail.com>
2022-01-23 16:14:06 -0500

    update installs add code in index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { sequelize } = require('../../models/Product');
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
@@ -8,3 +9,23 @@ router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
 module.exports = router;
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await seedCategories();
+    console.log('\n----- CATEGORIES SEEDED ----\n');
+
+    await seedProducts();
+    console.log('\n----- PRODUCTS SEEDED -----\n');
+
+    await seedTags();
+    console.log('\n----- TAGS SEEDED -----\n');
+
+    await seedProductsTags();
+    console.log('\n------ PRODUCTS TAGS SEEDED -----\n');
+
+    process.exit(0);
+};
+
+seedAll();
